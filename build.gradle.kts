83c164c20091de3851e5e622b89be64c7296ec5f
namndarmb-oss <namndarmb@gmail.com>
2026-09-15 16:19:42 +0430

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("com.android.application") version "8.5.0" apply false
-    kotlin("android") version "2.0.0" apply false
-    kotlin("kapt") version "2.0.0" apply false
-}
-
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
-}
