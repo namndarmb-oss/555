d044b959c374dcfa3da56a018a1c049dbed0e208
namndarmb-oss <namndarmb@gmail.com>
2026-09-15 16:42:41 +0430

    Delete .github/workflows/main.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,9 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     kotlin("android") version "2.0.0" apply false
     kotlin("kapt") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
 }
 
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
